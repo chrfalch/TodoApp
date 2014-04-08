07cf614cb4a8f6889984264246b4f564b1e0e010
chrfalch <christian.falch@gmail.com>
2014-04-08 13:27:15 +0200

    Added Shared Business Logic

---
+++ TodoApp/Shared/TodoApp.Shared.BL/IRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TodoApp.Shared.BL
+{
+
+    public interface IRepository<T> where T : IModel, new()
+    {
+        T CreateWithDefaults();
+        void Add(T entity);
+        void Remove(T entity);
+        void Update(T entity);
+        int Count { get; }
+
+    }
+}
