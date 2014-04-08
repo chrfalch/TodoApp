5243f9e9d5b84a94ba7fe687441aae9930824bf2
chrfalch <christian.falch@gmail.com>
2014-04-08 19:24:40 +0200

    Added implementation of binding and itemtemplate for the Items property

--- TodoApp/Clients/TodoApp.Clients.Core/ViewModels/TodoItemsViewModel.cs
+++ TodoApp/Clients/TodoApp.Clients.Core/ViewModels/TodoItemsViewModel.cs
@@ -1,3 +1,5 @@
+using System.Collections;
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using Cirrious.MvvmCross.ViewModels;
 using TodoApp.Shared.BL.Models;
@@ -8,17 +10,37 @@ namespace TodoApp.Clients.Core.ViewModels
     public class TodoItemsViewModel : MvxViewModel
     {
         private readonly ITodoItemUnitOfWork _todoItemUnitOfWork;
+        private string _newTodoItemText;
 
         public TodoItemsViewModel(ITodoItemUnitOfWork todoItemUnitOfWork)
         {
             _todoItemUnitOfWork = todoItemUnitOfWork;
         }
 
-        public ObservableCollection<ITodoItem> Items
+        public IEnumerable<ITodoItem> Items
+        {
+            get { return new List<ITodoItem>(_todoItemUnitOfWork.GetSortedItems()); }
+        }
+
+        public string NewTodoItemText
+        {
+            get { return _newTodoItemText; }
+            set
+            {
+                _newTodoItemText = value;
+                RaisePropertyChanged(() => NewTodoItemText);
+            }
+        }
+
+        public MvxCommand AddTodoItemCommand
         {
             get
             {
-                return new ObservableCollection<ITodoItem>(_todoItemUnitOfWork.GetSortedItems());
+                return new MvxCommand(() =>
+                {
+                    _todoItemUnitOfWork.CreateTodoItem(NewTodoItemText);
+                    RaisePropertyChanged(() => Items);
+                });
             }
         }
     }
