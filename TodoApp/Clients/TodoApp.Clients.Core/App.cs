00fbe0dc6dcbb331efb81b16210646c43c07d0a7
chrfalch <christian.falch@gmail.com>
2014-04-08 15:01:28 +0200

    Added Clients Core project - Added Repository Implementation - Added ViewModel construction

---
+++ TodoApp/Clients/TodoApp.Clients.Core/App.cs
@@ -0,0 +1,17 @@
+using Cirrious.CrossCore.IoC;
+
+namespace TodoApp.Clients.Core
+{
+    public class App : Cirrious.MvvmCross.ViewModels.MvxApplication
+    {
+        public override void Initialize()
+        {
+            CreatableTypes()
+                .EndingWith("Service")
+                .AsInterfaces()
+                .RegisterAsLazySingleton();
+				
+            RegisterAppStart<ViewModels.TodoItemsViewModel>();
+        }
+    }
+}
---
+++ TodoApp/Clients/TodoApp.Clients.Core/Repositories/Repository.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TodoApp.Shared.BL;
+
+namespace TodoApp.Clients.Core.Repositories
+{
+    public class Repository<T>: IRepository<T> where T: IModel
+    {
+        public IQueryable<T> Items()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Add(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Count { get; private set; }
+        public T GetById(string id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
