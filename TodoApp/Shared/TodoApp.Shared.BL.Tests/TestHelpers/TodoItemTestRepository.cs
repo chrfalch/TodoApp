21233fca017e2378536b9dec858e728ee80a4288
chrfalch <christian.falch@gmail.com>
2014-04-08 18:24:58 +0200

    Added interface and test implementation

---
+++ TodoApp/Shared/TodoApp.Shared.BL.Tests/TestHelpers/TodoItemTestRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TodoApp.Shared.BL.Models;
+using TodoApp.Shared.BL.Repositories;
+
+namespace TodoApp.Shared.BL.Tests.TestHelpers
+{
+    class TodoItemTestRepository: ListRepository<ITodoItem>, ITodoItemRepository
+    {
+    }
+}
---
+++ TodoApp/Shared/TodoApp.Shared.BL/Repositories/ITodoItemRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TodoApp.Shared.BL.Models;
+
+namespace TodoApp.Shared.BL.Repositories
+{
+    public interface ITodoItemRepository: IRepository<ITodoItem>
+    {
+    }
+}
