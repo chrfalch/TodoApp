53c18dc3bb86893ac6fb0b7204d44e454ddaaf5d
Christian Falch <christian.falch@gmail.com>
2014-04-10 08:27:46 +0200

    Implemented support for binding in iOS.

--- TodoApp/Clients/TodoApp.Clients.iPhone/Views/TodoItemsView.cs
+++ TodoApp/Clients/TodoApp.Clients.iPhone/Views/TodoItemsView.cs
@@ -5,6 +5,9 @@ using MonoTouch.ObjCRuntime;
 using MonoTouch.UIKit;
 using MonoTouch.Foundation;
 using TodoApp.Clients.Core.ViewModels;
+using Cirrious.MvvmCross.Binding.Touch.Views;
+using TodoApp.Shared.BL.Models;
+using System.Windows.Input;
 
 namespace TodoApp.Clients.iPhone.Views
 {
@@ -12,22 +15,48 @@ namespace TodoApp.Clients.iPhone.Views
     {
         public override void ViewDidLoad()
         {
-            View = new UIView(){ BackgroundColor = UIColor.White};
-            base.ViewDidLoad();
+			View = new UIView(){ BackgroundColor = UIColor.White};
+			base.ViewDidLoad();
 
 			// ios7 layout
-            if (RespondsToSelector(new Selector("edgesForExtendedLayout")))
-               EdgesForExtendedLayout = UIRectEdge.None;
-			   
-            var label = new UILabel(new RectangleF(10, 10, 300, 40));
-            Add(label);
-            var textField = new UITextField(new RectangleF(10, 50, 300, 40));
-            Add(textField);
+			if (RespondsToSelector(new Selector("edgesForExtendedLayout")))
+				EdgesForExtendedLayout = UIRectEdge.None;
+
+			var textField = new UITextField(new RectangleF(0, 0, 280, 40));
+			textField.BorderStyle = UITextBorderStyle.Line;
+			Add(textField);
+
+			var button = new UIButton (UIButtonType.RoundedRect);
+			button.Frame = new RectangleF (280, 0, 40, 40);
+			button.SetTitle ("+", UIControlState.Normal);
+			button.TouchUpInside += (object sender, System.EventArgs e) => {
+				(ViewModel as TodoItemsViewModel).AddTodoItemCommand.Execute();
+			};
+
+			Add (button);
+
+			var tableView = new UITableView(new RectangleF(0, 40, 320, 500), UITableViewStyle.Plain);
+			Add(tableView);
+
+			// choice here:
+			//
+			//   for original demo use:
+			//     var source = new MvxStandardTableViewSource(tableView, "TitleText");
+			//
+			//   or for prettier cells from XIB file use:
+			//     tableView.RowHeight = 88;
+			//     var source = new MvxSimpleTableViewSource(tableView, BookCell.Key, BookCell.Key);
+
+			tableView.RowHeight = 88;
+			var source = new MvxStandardTableViewSource (tableView, "TitleText Text;");
+			tableView.Source = source;
 
 			var set = this.CreateBindingSet<TodoItemsView, TodoItemsViewModel>();
-			set.Bind(label).To(vm => vm.NewTodoItemText);
 			set.Bind(textField).To(vm => vm.NewTodoItemText);
-            set.Apply();
+			set.Bind(source).To(vm => vm.Items);
+			set.Apply();
+
+			tableView.ReloadData();
         }
     }
 }
